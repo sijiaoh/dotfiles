aafd2a81b788f605b9911032dc1dbcffcd378b2e
Sijia Wang <sijiaoh@outlook.com>
2017-06-03 23:21:23 +0900

    Add prezto

---
+++ zsh/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+ln -sfv ${DOTFILES_ROOT}/zsh/init.zsh ${ZDOTDIR:-$HOME}/.zprezto/init.zsh
