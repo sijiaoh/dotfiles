2b3a62ddd8abddbdf522eecf67ba70fc22a7e9dc
Sijia Wang <sijiaoh@outlook.com>
2017-06-04 01:27:16 +0900

    Fix zsh custom file name

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,5 +6,3 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
-
-ln -sfv ${DOTFILES_ROOT}/zsh/init.zsh ${ZDOTDIR:-$HOME}/.zprezto/init.zsh
