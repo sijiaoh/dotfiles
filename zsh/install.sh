f694f792a7e0bf5a4323581ea1ddeccab0132acd
Sijia Wang <sijiaoh@outlook.com>
2017-06-04 11:56:19 +0900

    Fix zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env zsh
 
+set -eu
+
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+
+ln -sfv ${DOTFILES_ROOT}/zsh/zshrc.zsh ${ZDOTDIR:-$HOME}/.zshrc
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,4 +1,7 @@
-#!/usr/bin/env zsh
+# Source Prezto.
+if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
+  source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+fi
 
 export DOTFILES_ROOT=$(cat ${HOME}/.dotfiles_root)
 
