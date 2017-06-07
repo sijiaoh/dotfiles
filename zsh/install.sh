7aa7b460c45108405fd7ddd664999e085e2a6d4a
Sijia Wang <sijiaoh@outlook.com>
2017-06-07 10:22:31 +0900

    Fix zsh ln options

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,7 +8,7 @@ git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$H
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  ln -sfv "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
 ln -sfv ${DOTFILES_ROOT}/zsh/zshrc.zsh ${ZDOTDIR:-$HOME}/.zshrc
