a424330ace5de2379804a14e443336b830cfd36f
Sijia Wang <sijiaoh@outlook.com>
2017-06-17 23:29:28 +0900

    Add install ruby
    With bundle and google-ime-skk

--- rbenv/install.sh
+++ rbenv/install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -eu
 
+RUBY_VERSION=2.4.1
+
 brew install rbenv
+rbenv install ${RUBY_VERSION}
+rbenv global ${RUBY_VERSION}
+
+eval "$(rbenv init -)"
+
+gem install bundle
+gem install google-ime-skk
