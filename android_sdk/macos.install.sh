c1dabcdade1f638a361e739567035b5614b609aa
Sijia Wang <sijiaoh@outlook.com>
2017-06-24 12:04:19 +0900

    Add Android SDK

---
+++ android_sdk/macos.install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -eu
+
+brew cask install java
+brew cask install android-sdk
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -22,3 +22,7 @@ eval "$(direnv hook zsh)"
 
 export GOPATH=${HOME}/work/go
 export PATH=${GOPATH}/bin:${PATH}
+
+if test ${OS} = 'macos'; then
+  export ANDROID_HOME=/usr/local/Caskroom/android-sdk/$(brew cask info android-sdk | head -1 | tr ' ' '\n' | tail -1)
+fi
