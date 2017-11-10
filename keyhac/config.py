07e85983374efe0eae76a8f8b0c0e9a6d0b5839f
Sijia Wang <sijiaoh@outlook.com>
2017-11-10 14:56:59 +0900

    keyhac のレスポンスが良くなってる？ため、いろんなキーを置き換える

--- keyhac/config.py
+++ keyhac/config.py
@@ -5,19 +5,18 @@ def configure(keymap):
 
     keymap_global[ "Ctrl-M" ] = "Return"
     keymap_global[ "Ctrl-Shift-M" ] = "Shift-Return"
-    # keymap_global[ "Ctrl-H" ] = "Back"
+    keymap_global[ "Ctrl-H" ] = "Back"
     keymap_global[ "Alt-H" ] = "Alt-Back"
-    # keymap_global[ "Ctrl-D" ] = "Delete"
+    keymap_global[ "Ctrl-D" ] = "Delete"
     keymap_global[ "Alt-D" ] = "Alt-Delete"
     keymap_global[ "Ctrl-I" ] = "Tab"
     keymap_global[ "Ctrl-Shift-I" ] = "Shift-Tab"
-    # keymap_global[ "Ctrl-P" ] = "Up"
-    # keymap_global[ "Ctrl-N" ] = "Down"
-    # keymap_global[ "Ctrl-F" ] = "Right"
-    # keymap_global[ "Ctrl-B" ] = "Left"
-    # keymap_global[ "Alt-F" ] = "Alt-Right"
-    # keymap_global[ "Alt-B" ] = "Alt-Left"
-    # keymap_global[ "Ctrl-S" ] = "Alt-F"
+    keymap_global[ "Ctrl-P" ] = "Up"
+    keymap_global[ "Ctrl-N" ] = "Down"
+    keymap_global[ "Ctrl-B" ] = "Left"
+    keymap_global[ "Ctrl-F" ] = "Right"
+    keymap_global[ "Alt-F" ] = "Alt-Right"
+    keymap_global[ "Alt-B" ] = "Alt-Left"
     keymap_global[ "Ctrl-G" ] = "Esc"
     keymap_global[ "Ctrl-OpenBracket" ] = "Esc"
     # 左 Command 単独押しで英数
@@ -40,6 +39,7 @@ def configure(keymap):
     for app in ["com.googlecode.iterm2"]:
         keymap_local[app] = keymap.defineWindowKeymap(app_name=app)
 
+        keymap_local[app][ "Ctrl-H" ] = "Ctrl-H"
         keymap_local[app][ "Alt-H" ] = "Alt-H"
         keymap_local[app][ "Alt-D" ] = "Alt-D"
         keymap_local[app][ "Ctrl-G" ] = "Ctrl-G"
