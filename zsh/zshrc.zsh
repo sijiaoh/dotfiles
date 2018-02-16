1b97b6e5bfd95361b979c8404365cc92d90f973d
Sijia Wang <sijiaoh@outlook.com>
2018-02-16 11:22:29 +0900

    fzf 関連の処理を別ファイルに切り出す

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -43,67 +43,7 @@ alias -g XG='| xargs grep'
 
 bindkey '^[h' backward-kill-word
 
-export FZF_DEFAULT_OPTS="--bind 'ctrl-k:kill-line'"
-
-fe() {
-  local files
-  IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
-  [[ -n "$files" ]] && eval "emacsclient --no-wait ${files[@]}"
-}
-
-fv() {
-  local files
-  IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
-  [[ -n "$files" ]] && eval "nvim ${files[@]}"
-}
-
-fd() {
-  local dir
-  dir=$(find ${1:-.} -path '*/\.*' -prune \
-    -o -type d -print 2> /dev/null | fzf-tmux +m) &&
-    cd "$dir"
-}
-
-fda() {
-  local dir
-  dir=$(find ${1:-.} -type d 2> /dev/null | fzf-tmux +m) && cd "$dir"
-}
-
-fdd() {
-  local declare dirs=()
-  get_parent_dirs() {
-    if [[ -d "${1}" ]]; then dirs+=("$1"); else return; fi
-    if [[ "${1}" == '/' ]]; then
-      for _dir in "${dirs[@]}"; do echo $_dir; done
-    else
-      get_parent_dirs $(dirname "$1")
-    fi
-  }
-  local DIR=$(get_parent_dirs $(realpath "${1:-$PWD}") | fzf-tmux)
-  cd "$DIR"
-}
-
-fge() {
-  local file
-
-  file="$(git grep --line-number $@ | fzf-tmux -0 -1 | awk -F: '{print "+" $2 " ./" $1}')"
-
-  if [[ -n $file ]]
-  then
-    emacsclient --no-wait $(echo $file)
-  fi
-}
-
-fgv() {
-  local file
-
-  file="$(ag --nobreak --noheading $@ | fzf-tmux -0 -1 | awk -F: '{print $1 " +" $2}')"
-
-  if [[ -n $file ]]
-  then
-    nvim $file
-  fi
-}
+source ${DOTFILES_ROOT}/zsh/fzf.zsh
 
 alias a='atom'
 
