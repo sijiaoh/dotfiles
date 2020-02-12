f9d77bd6aa6b72e3717704bd2f6be045dd57810a
Sijia Wang <sijiaoh@outlook.com>
2020-02-12 16:46:49 +0900

    Add anyenv-update.

--- anyenv/install.sh
+++ anyenv/install.sh
@@ -7,6 +7,11 @@ fi
 PATH="$HOME/.anyenv/bin:$PATH"
 eval "$(anyenv init -)"
 
+mkdir -p $(anyenv root)/plugins
+if [ ! -e $(anyenv root)/plugins/anyenv-update ]; then
+  git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
+fi
+
 anyenv install rbenv
 anyenv install pyenv
 anyenv install nodenv
