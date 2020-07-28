73dc4eaf9c036769993ed96a4b94c32d059e8750
Sijia Wang <sijiaoh@outlook.com>
2020-07-28 05:02:15 +0000

    Remove install *env codes.

--- anyenv/install.sh
+++ anyenv/install.sh
@@ -12,7 +12,3 @@ mkdir -p $(anyenv root)/plugins
 if [ ! -e $(anyenv root)/plugins/anyenv-update ]; then
   git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
 fi
-
-anyenv install rbenv
-anyenv install pyenv
-anyenv install nodenv
