9f061934d36e7b8352ec3aab0b34147f23ccd625
Sijia Wang <sijiaoh@outlook.com>
2021-02-25 22:31:31 +0900

    Install vim-plug in neovim/install.sh.

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -eu
 
+# Install vim-plug.
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 mkdir -p ${ZDOTDIR:-$HOME}/.config/nvim
 ln -sfv ${DOTFILES_ROOT}/neovim/init.vim ${ZDOTDIR:-$HOME}/.config/nvim/init.vim
