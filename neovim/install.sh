8cb93e839ed79bdf163bb98bca3d37241d8a7e9d
Sijia Wang <sijiaoh@outlook.com>
2021-02-26 10:31:44 +0900

    Add win32yank installer to neovim.

--- neovim/install.sh
+++ neovim/install.sh
@@ -4,6 +4,12 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+curl -fLO https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+unzip win32yank-x64.zip -d win32yank
+chmod u+x win32yank/win32yank.exe
+sudo mv win32yank/win32yank.exe /usr/local/bin/win32yank
+rm -rf win32yank-x64.zip win32yank
+
 curl -fLO https://github.com/iuchim/zenhan/releases/download/v0.0.1/zenhan.zip
 unzip zenhan.zip
 chmod u+x zenhan/bin64/zenhan.exe
