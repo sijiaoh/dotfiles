c540f9a0ae790ef0563bce35691bf9a3fd0ae3e6
Sijia Wang <sijiaoh@outlook.com>
2022-04-04 17:15:57 +0900

    Add osx brew path setting.

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -22,6 +22,8 @@ if test ${OS} = 'linux'; then
   export PATH="/home/linuxbrew/.linuxbrew/bin:${PATH}"
   export MANPATH="/home/linuxbrew/.linuxbrew/share/man:${MANPATH}"
   export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:${INFOPATH}"
+else
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Source Prezto.
