89bf9084974d7a87534c4ebb483611d0b756461f
Sijia Wang <sijiaoh@outlook.com>
2022-04-30 09:39:05 +0900

    direnvディレクトリから抜けると環境変数がおかしくなる問題を修正。

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,14 +1,3 @@
-if type "direnv" > /dev/null 2>&1; then
-  eval "$(direnv export zsh)"
-fi
-
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
 export EDITOR='vim'
 
 export DOTFILES_ROOT=$(cat ${HOME}/.dotfiles_root)
