339892b4caecd81441d79fe523d1dd287d732c6f
sijiaoh <sijiaoh@outlook.com>
2022-10-18 10:43:06 +0900

    Fix install_brew failed on mac.

--- lib/install_brew.sh
+++ lib/install_brew.sh
@@ -1,1 +1,6 @@
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+. ${DOTFILES_ROOT}/lib/os.sh
+
+if test ${OS} = "macos"; then
+  SUDO=1
+fi
+NONINTERACTIVE=1 /bin/bash -c "$(${SUDO:+sudo} curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
