01c871cc6fcb909923322cc462b0da9bd79bb345
sijiaoh <sijiaoh@outlook.com>
2024-02-29 21:34:13 +0900

    Change anyenv/setup.lua to ruby.

--- anyenv/setup.lua
+++
@@ -1,9 +0,0 @@
-local anyenv_root = "~/.anyenv"
-
-if not CheckFileExists(anyenv_root) then
-  assert(GitClone(anyenv_root, "https://github.com/anyenv/anyenv"))
-  assert(Exec(anyenv_root .. "/bin/anyenv install --force-init"))
-end
-
-MkdirP(anyenv_root .. "/plugins")
-assert(GitClone(anyenv_root .. "/plugins/anyenv-update", "https://github.com/znz/anyenv-update.git"))
