c6440e9addb83412ef474f60c18396ca15c6fbc6
sijiaoh <sijiaoh@outlook.com>
2024-02-29 21:56:18 +0900

    Fix File.mkdir missing.

--- anyenv/setup.rb
+++ anyenv/setup.rb
@@ -3,6 +3,6 @@ anyenv_root = "~/.anyenv"
 unless File.exist? anyenv_root
   Utils.git_clone path: anyenv_root, url: "https://github.com/anyenv/anyenv.git"
   Utils.exec "#{File.join(anyenv_root, 'bin/anyenv')} install --force-init"
-  File.mkdir File.join(anyenv_root, "plugins")
+  Dir.mkdir File.join(anyenv_root, "plugins")
   Utils.git_clone path: File.join(anyenv_root, "plugins/anyenv-update"), url: "https://github.com/znz/anyenv-update.git"
 end
