05e4377d8ffe6a2524a945cbcc894258fdaebaaa
sijiaoh <sijiaoh@outlook.com>
2024-03-05 18:43:29 +0900

    Fix C-l not working in vim terminal.

--- nvim/lazyvim/lua/config/keymaps.lua
+++ nvim/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Unmap C-hjkl in terminal.
+vim.cmd([[
+  tunmap <C-h>
+  tunmap <C-j>
+  tunmap <C-k>
+  tunmap <C-l>
+]])
