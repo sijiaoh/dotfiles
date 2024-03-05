e88b223d737bf58e6292eb2dac4988a2e5b5daa2
sijiaoh <sijiaoh@outlook.com>
2024-03-05 15:58:56 +0900

    Fix Utils#create_symlink will try to remove file when to is nil.

--- src/utils.rb
+++ src/utils.rb
@@ -14,10 +14,10 @@ class Utils
     end
 
     def create_symlink(from, to = nil)
-      absolute_to = File.expand_path to
-      FileUtils.rm_rf absolute_to
-
       unless to.nil?
+        absolute_to = File.expand_path to
+        FileUtils.rm_rf absolute_to
+
         absolute_to_dir = File.dirname to
         FileUtils.mkdir_p absolute_to_dir
       end
