61e5bd46e432a0550f0b8a713189465158aa058b
sijiaoh <sijiaoh@outlook.com>
2024-03-05 22:10:27 +0900

    Add border to floating terminal.

--- nvim/lazyvim/lua/config/keymaps.lua
+++ nvim/lazyvim/lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local Util = require("lazyvim.util")
+local map = vim.keymap.set
+
 -- Unmap C-hjkl in terminal.
 vim.cmd([[
   tunmap <C-h>
@@ -9,3 +12,11 @@ vim.cmd([[
   tunmap <C-k>
   tunmap <C-l>
 ]])
+
+-- Add border to floating terminal.
+local lazyterm = function()
+  Util.terminal(nil, { cwd = Util.root(), border = "rounded" })
+end
+map("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
+map("n", "<C-/>", lazyterm, { desc = "Terminal (root dir)" })
+map("n", "<C-_>", lazyterm, { desc = "which_key_ignore" })
