b50f940fc8e959297a56d4947c0514152f77f77e
sijiaoh <sijiaoh@outlook.com>
2024-06-17 12:06:10 +0900

    Add lt lta aliases.

--- eza/eza.zsh
+++ eza/eza.zsh
@@ -1,4 +1,6 @@
-alias ls='eza --group-directories-first --icons'
+alias ls='eza --group-directories-first --icons --git'
+alias lt='ls --tree --level=2 --long --header'
+alias lta='lt --all'
 
 function chpwd() {
   ls
