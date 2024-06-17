8069e97ad5c860b0bfbbfb061bf7aeeb68b0ab34
sijiaoh <sijiaoh@outlook.com>
2024-06-17 15:06:36 +0900

    Enable pager to ll and la.

--- eza/eza.zsh
+++ eza/eza.zsh
@@ -1,7 +1,11 @@
 alias ls='eza --group-directories-first --icons --git'
-alias ltbase='ls --tree --level=2 --long --header --color=always'
-alias lt='ltbase | cat --plain'
-alias lta='ltbase --all | cat --plain'
+alias llbase='ls --long --header --icons=always --color=always'
+alias ll='llbase L'
+alias la='llbase --all L'
+
+alias ltbase='llbase --tree --level=2'
+alias lt='ltbase L'
+alias lta='ltbase --all L'
 
 function chpwd() {
   ls
