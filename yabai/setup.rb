05ca86e376710319f9cef06190243985595dc027
sijiaoh <sijiaoh@outlook.com>
2024-08-06 04:18:27 +0900

    Fix yabai/setup.rb breaks symlinks.

--- yabai/setup.rb
+++ yabai/setup.rb
@@ -1,6 +1,3 @@
 Utils.brew_install "jq"
 Utils.brew_install "koekeishiya/formulae/yabai"
 Utils.brew_install "koekeishiya/formulae/skhd"
-
-Utils.create_symlink ".yabairc"
-Utils.create_symlink ".skhdrc"
