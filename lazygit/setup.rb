529e18a927432cdd72a7c59c6314afa8fb532049
sijiaoh <sijiaoh@outlook.com>
2024-08-20 02:35:35 +0900

    Change lazygit pager to delta.

--- lazygit/setup.rb
+++ lazygit/setup.rb
@@ -1,2 +1,8 @@
 Utils.exec "./lazygit/linux.sh" if Utils.linux?
 Utils.brew_install "lazygit"
+
+if Utils.linux?
+  Utils.create_symlink "lazygit/config.yml", "~/.config/lazygit/config.yml"
+else
+  Utils.create_symlink "lazygit/config.yml", "~/Library/Application\\ Support/lazygit/config.yml"
+end
