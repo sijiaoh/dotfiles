12927ff0eb3368fb8462b1ecbe4ccf0be89697d3
sijiaoh <sijiaoh@outlook.com>
2024-11-07 21:07:54 +0900

    Fix error when batcat exists instead of bat.

--- bat/bat.zsh
+++ bat/bat.zsh
@@ -1,8 +1,8 @@
 if command -v batcat &>/dev/null; then
   alias cat='batcat --style=auto'
+  alias less='batcat'
 else
   alias cat='bat --style=default'
+  alias less='bat'
+  export PAGER='bat'
 fi
-alias less='bat'
-
-export PAGER='bat'
