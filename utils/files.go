f00d2a10d99bcf57fffddb23100e069d20417e9e
sijiaoh <sijiaoh@outlook.com>
2025-01-18 17:48:31 +0900

    utils内のファイル構造をリファクタリング

--- utils/files.go
+++ utils/files.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"io"
 	"os"
+	"path"
 )
 
 func MkdirP(dir string) {
@@ -25,6 +26,26 @@ func CopyFileTo(src, dest string) {
 	defer destFile.Close()
 
 	_, err = io.Copy(destFile, srcFile)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// src: 自動でDotfilesRoot()が付与される
+func CreateSymlink(src, dest string) {
+	absoluteSrc := path.Join(DotfilesRoot(), src)
+
+	if dest[:2] == "~/" {
+		dest = path.Join(os.Getenv("HOME"), dest[2:])
+	}
+
+	if _, err := os.Stat(dest); err == nil {
+		if err := os.Remove(dest); err != nil {
+			panic(err)
+		}
+	}
+
+	_, err := ExecCommand("ln", "-sf", absoluteSrc, dest)
 	if err != nil {
 		panic(err)
 	}
