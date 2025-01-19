41a28dfe080d2c0417b659c511c0a80da5654b85
sijiaoh <sijiaoh@outlook.com>
2025-01-20 05:20:42 +0900

    Disable C-hjkl in terminal.

--- nvim/lazyvim/lua/plugins/snacks.lua
+++ nvim/lazyvim/lua/plugins/snacks.lua
@@ -7,11 +7,10 @@ return {
     terminal = {
       win = {
         keys = {
-          -- <C-hjkl>は通常通り使用したい
-          -- nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
-          -- nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
-          -- nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
-          -- nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
+          nav_h = { "<C-h>", false },
+          nav_j = { "<C-j>", false },
+          nav_k = { "<C-k>", false },
+          nav_l = { "<C-l>", false },
         },
       },
     },
