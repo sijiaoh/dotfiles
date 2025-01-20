a6659ede57999ad8670c23f79e340065b44a297e
sijiaoh <sijiaoh@outlook.com>
2025-01-21 01:36:44 +0900

    Add Emacs like keybindings in normal and command mode.

--- nvim/lazyvim/lua/config/keymaps.lua
+++ nvim/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- Emacs like keybindings
+map({ "i", "c" }, "<C-p>", "<Up>")
+map({ "i", "c" }, "<C-n>", "<Down>")
+map({ "i", "c" }, "<C-f>", "<Right>")
+map({ "i", "c" }, "<C-b>", "<Left>")
+map({ "i", "c" }, "<C-a>", "<Home>")
+map({ "i", "c" }, "<C-e>", "<End>")
