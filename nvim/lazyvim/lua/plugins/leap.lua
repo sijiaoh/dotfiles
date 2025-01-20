ec36e50ab31a1a52aacf5eb6a5409f60ad52ac36
sijiaoh <sijiaoh@outlook.com>
2025-01-21 02:12:49 +0900

    Disable leap default mappings.

--- nvim/lazyvim/lua/plugins/leap.lua
+++ nvim/lazyvim/lua/plugins/leap.lua
@@ -1,18 +1,27 @@
 return {
+  -- From: http://www.lazyvim.org/extras/editor/leap#leapnvim
   {
     "ggandor/leap.nvim",
+    enabled = true,
+    opts = {
+      -- Remove s key from labels.
+      -- From `:help leap.opts.safe_labels`
+      safe_labels = "fnut/FNLHMUGTZ?",
+      labels = "fnjklhodweimbuyvrgtaqpcxz/FNJKLHODWEIMBUYVRGTAQPCXZ?",
+    },
     keys = {
       -- EasyMotion like keybinds.
       { "<leader><space>f", "<Plug>(leap-forward)", desc = "Leap forward to" },
       { "<leader><space>F", "<Plug>(leap-backward)", desc = "Leap backward to" },
       { "<leader><space>g", "<Plug>(leap-from-window)", desc = "Leap from window" },
     },
-    init = function()
-      -- Do not customize sSfF.
-      vim.cmd("unmap s")
-      vim.cmd("unmap S")
-      vim.cmd("unmap f")
-      vim.cmd("unmap F")
+    config = function(_, opts)
+      local leap = require("leap")
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      vim.keymap.del({ "x", "o" }, "x")
+      vim.keymap.del({ "x", "o" }, "X")
     end,
   },
 }
