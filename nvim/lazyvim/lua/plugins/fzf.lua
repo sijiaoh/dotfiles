09f62d364c9d7599d2754fad9814d8bc08f849c1
sijiaoh <sijiaoh@outlook.com>
2025-01-21 13:11:15 +0900

    Enable emacs like keybindings in find file window.

--- nvim/lazyvim/lua/plugins/fzf.lua
+++ nvim/lazyvim/lua/plugins/fzf.lua
@@ -9,5 +9,16 @@ return {
       -- CtrlP like keybind.
       { "<C-p>", Util.pick("files"), desc = "Find Files (root dir)" },
     },
+    opts = function()
+      local fzf = require("fzf-lua")
+      local config = fzf.config
+
+      -- Emacs like keybindings
+      config.defaults.keymap.fzf["ctrl-f"] = "forward-char"
+      config.defaults.keymap.fzf["ctrl-b"] = "backward-char"
+      config.defaults.keymap.fzf["ctrl-d"] = "delete-char"
+      config.defaults.keymap.builtin["<c-f>"] = false
+      config.defaults.keymap.builtin["<c-b>"] = false
+    end,
   },
 }
