343fc8af42b9220eb9b6b5abb726eb25e65aaaf6
sijiaoh <sijiaoh@outlook.com>
2025-01-24 07:59:29 +0000

    Disable flash in visual mode.

--- nvim/lazyvim/lua/plugins/flash.lua
+++ nvim/lazyvim/lua/plugins/flash.lua
@@ -4,8 +4,8 @@ return {
     event = "VeryLazy",
     -- stylua: ignore
     keys = {
-      { "s", false },
-      { "S", false },
+      { "s", mode={ "n", "x", "o" }, false },
+      { "S", mode={ "n", "x", "o" }, false },
       { "<leader><space>f", mode = { "n" }, function() require("flash").jump() end, desc = "Flash" },
       { "<leader><space>F", mode = { "n" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     },
