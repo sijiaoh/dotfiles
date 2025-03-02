a39c6b91348a522b1e4be3d13a659e024ce10042
sijiaoh <sijiaoh@outlook.com>
2025-03-02 06:21:08 +0000

    Customize flash.nvim

--- nvim/lazyvim/lua/plugins/flash.lua
+++ nvim/lazyvim/lua/plugins/flash.lua
@@ -9,6 +9,30 @@ return {
       { "<leader><space>f", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       { "<leader><space>F", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     },
+    opts = {
+      highlight = { backdrop = false },
+      modes = {
+        search = {
+          highlight = { backdrop = false },
+        },
+        char = {
+          char_actions = function()
+            return {
+              [";"] = "next", -- set to `right` to always go right
+              [","] = "prev", -- set to `left` to always go left
+
+              -- Disable repeat motion keys
+              -- [motion:lower()] = "next",
+              -- [motion:upper()] = "prev",
+            }
+          end,
+          highlight = { backdrop = false },
+        },
+        treesitter = {
+          highlight = { backdrop = false },
+        },
+      },
+    },
   },
   {
     "folke/which-key.nvim",
