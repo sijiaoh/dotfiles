32ab371eb42780960b38c6a0b32d133b0a49fa54
sijiaoh <sijiaoh@outlook.com>
2025-04-16 20:57:49 +0000

    Add comment to yanky.lua

--- nvim/lazyvim/lua/plugins/yanky.lua
+++ nvim/lazyvim/lua/plugins/yanky.lua
@@ -4,6 +4,8 @@ return {
     opts = {
       picker = {
         select = {
+          -- LazyVimとyanky.nvimの組み合わせだとYankyRingHistoryがカーソル左にペーストされてしまう
+          -- https://github.com/gbprod/yanky.nvim/issues/219
           action = function(...)
             vim.cmd("normal! l")
             require("yanky.picker").actions.put("p")(...)
