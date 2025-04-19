3841f82606d6be24ae5d4231ff44a9f4af924f86
sijiaoh <sijiaoh@outlook.com>
2025-04-19 16:09:44 +0000

    Neovimのバージョンをv0.10.4に固定

---
+++ nvim/lazyvim/lua/plugins/grug-far.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "MagicDuck/grug-far.nvim",
+    -- For Neovim v1.10.4
+    tag = "1.6.3",
+  },
+}
--- nvim/setup.go
+++ nvim/setup.go
@@ -11,7 +11,7 @@ func Setup() {
 	if utils.IsLinux() && utils.IsAmd64() {
 		tarPath := "./nvim-linux-x86_64.tar.gz"
 		dirPath := strings.TrimSuffix(tarPath, ".tar.gz")
-		tarUrl := "https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz"
+		tarUrl := "https://github.com/neovim/neovim/releases/v0.10.4/download/nvim-linux-x86_64.tar.gz"
 
 		utils.MustExecCommand(fmt.Sprintf("curl -L %s -o %s", tarUrl, tarPath))
 		defer utils.MustExecCommand("rm -f " + tarPath)
@@ -28,7 +28,7 @@ func Setup() {
 			"https://github.com/neovim/neovim",
 			"/tmp/neovim",
 			&utils.GitCloneOptions{
-				Tag:   utils.ToStringPtr("v0.11.0"),
+				Tag:   utils.ToStringPtr("v0.10.4"),
 				Depth: utils.ToIntPtr(1),
 			},
 		)
