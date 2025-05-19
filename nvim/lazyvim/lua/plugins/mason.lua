038741919296a894468200ed8fdff1906a229f4f
sijiaoh <sijiaoh@outlook.com>
2025-05-19 07:18:50 +0000

    Fix mason error message

---
+++ nvim/lazyvim/lua/plugins/mason.lua
@@ -0,0 +1,6 @@
+return {
+  -- configが見つからないエラーの対処のためにバージョンを固定
+  -- https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
