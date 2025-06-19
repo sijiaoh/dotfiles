698e7075d3414f50fa3b974b172de8caa19fb541
sijiaoh <sijiaoh@outlook.com>
2025-06-19 14:40:22 +0900

    Fix godot alias nvim path

--- nvim/nvim.zsh
+++ nvim/nvim.zsh
@@ -7,8 +7,8 @@ alias v='nvim --listen /tmp/nvim-server.pipe'
 compdef v='nvim'
 
 # Neovim with Godot integration
-alias gv='/usr/local/bin/nvim --listen /tmp/nvim-godot-server.pipe'
+alias gv='nvim --listen /tmp/nvim-godot-server.pipe'
 # Configure Godot
 # Editor Settings... -> Text Editor -> External
-# - Exec Path: /usr/local/bin/nvim
+# - Exec Path: nvim
 # - Exec Flags: --server /tmp/nvim-godot-server.pipe --remote-send "<C-\><C-N>:n {file}<CR>{line}G{col}|"
