6aa0dee2045091bcc78ce69f035d161f69ff869b
sijiaoh <sijiaoh@outlook.com>
2025-06-21 01:28:49 +0900

    Update godot nvim path

--- nvim/nvim.zsh
+++ nvim/nvim.zsh
@@ -10,5 +10,5 @@ compdef v='nvim'
 alias gv='nvim --listen /tmp/nvim-godot-server.pipe'
 # Configure Godot
 # Editor Settings... -> Text Editor -> External
-# - Exec Path: nvim
+# - Exec Path: /usr/local/bin/nvim
 # - Exec Flags: --server /tmp/nvim-godot-server.pipe --remote-send "<C-\><C-N>:n {file}<CR>{line}G{col}|"
