6d0598afc6b4f24053fc88859d5b2a5d06ba93aa
sijiaoh <sijiaoh@outlook.com>
2025-06-21 18:30:08 +0900

    Fix neovim not respond to C-/ in wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -14,5 +14,13 @@ config.enable_tab_bar = false
 config.use_ime = true
 config.macos_forward_to_ime_modifier_mask = "SHIFT|CTRL"
 
+config.keys = {
+  -- NeovimがC-/を正しく処理できないとのこと
+  -- From: https://github.com/wezterm/wezterm/issues/3180
+  --
+  -- TODO: これは最適な解決法ではなさそう。上記PRが更新されたら別の解決法が見つかるかもしれない。
+  { key = "/", mods = "CTRL", action = wezterm.action.SendString("\x1f") },
+}
+
 -- Finally, return the configuration to wezterm:
 return config
