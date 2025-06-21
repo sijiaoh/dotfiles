33e9b3acabe762fc0609ce0923d08728338bbe8c
sijiaoh <sijiaoh@outlook.com>
2025-06-21 13:04:21 +0000

    Add reverse_ports to support godot lsp port

--- devcontainer/devcontainer.zsh
+++ devcontainer/devcontainer.zsh
@@ -2,14 +2,19 @@ alias devcreate='cp -rf ~/.dotfiles/devcontainer .devcontainer'
 
 function devup() {
   pkill -f autossh
-  local ports=$(cat .devcontainer/forward_ports)
+  local forward_ports=$(cat .devcontainer/forward_ports)
 
-  for port in $=ports; do
+  for port in $=forward_ports; do
     if nc -z localhost $port; then
-      echo "Port $port is already in use"
+      echo "Local port $port is already in use"
       return 1
     fi
-    autossh -M 0 -f -N -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null -L ${port}:localhost:${port} -p 2222 vscode@localhost
+    autossh -M 0 -f -N -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null -L localhost:${port}:localhost:${port} -p 2222 vscode@localhost
+  done
+
+  local reverse_ports=$(cat .devcontainer/reverse_ports)
+  for port in $=reverse_ports; do
+    autossh -M 0 -f -N -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null -R localhost:${port}:localhost:${port} -p 2222 vscode@localhost
   done
 
   devcontainer up --workspace-folder=. --dotfiles-repository=git@github.com:sijiaoh/dotfiles.git --dotfiles-target-path='~/.dotfiles' --dotfiles-install-command=bootstrap $@
