32ca9a78245eee81dbfe8c4cb6ede30c636114fa
sijiaoh <sijiaoh@outlook.com>
2025-11-22 12:53:14 +0900

    zoxideを対話シェルでのみ有効にする

--- zoxide/zoxide.zsh
+++ zoxide/zoxide.zsh
@@ -1,3 +1,6 @@
-eval "$(zoxide init zsh)"
-
-alias cd=z
+# 対話シェルのときのみ有効にする
+# claude code内からの呼び出しに失敗するため
+# https://github.com/anthropics/claude-code/issues/2407
+if [[ $- == *i* ]]; then
+  eval "$(zoxide init zsh --cmd cd)"
+fi
